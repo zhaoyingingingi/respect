5452bae9a3767399aed74b850a5a8cb4a2625037
WIN-RARQFV76KST\Administrator <2212863484@qq.com>
2023-11-17 15:03:03 +0800

    1

---
+++ pythonGUI/canvas.py
@@ -0,0 +1,27 @@
+import tkinter as tk
+
+window=tk.Tk()
+window.title('my window')
+window.geometry('200x200')
+
+canvas=tk.Canvas(window,bg='blue',height=100,width=200)
+image_file=tk.PhotoImage(file='1.png')
+image=canvas.create_image(0,0,anchor='nw',image=image_file)
+
+x0,y0,x1,y1=50,50,80,80
+line=canvas.create_line(x0,y0,x1,y1)
+
+oval=canvas.create_oval(x0,y0,x1,y1,fill='red')
+
+arc=canvas.create_arc(x0+30,y0+30,x1+30,y1+30,start=0,extent=90)
+
+rect=canvas.create_rectangle(100,30,100+20,30+20)
+canvas.pack()
+
+def moveit():
+    canvas.move(rect,0,2)
+
+b=tk.Button(window,text='Move',command=moveit)
+b.pack()
+
+window.mainloop()
